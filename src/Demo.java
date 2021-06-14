e629c4e6cb55c3187cd01f473a9b17196ed0a967
erkanozcelik19@gmail.com <Karpuz.47>
2021-06-14 19:21:40 -0400

    We have explain or and and in the if else condition

--- src/Demo.java
+++ src/Demo.java
@@ -1,2 +1,17 @@
 public class Demo {
+
+    public static void main(String[] args) {
+       int a =5;
+       int b = 3;
+
+        if( a==3   ||   a<3){
+            System.out.println();
+        }
+
+        if (a<3 && a<5){
+
+        }
+
+    }
+
 }
