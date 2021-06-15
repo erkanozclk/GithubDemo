d7273741daf48515579c1766a46d25575392787c
erkanozcelik19@gmail.com <Karpuz.47>
2021-06-14 20:45:07 -0400

    remaned Demo2

--- src/Demo3.java
+++ src/Demo3.java
@@ -1,3 +1,19 @@
 public class Demo3 {
     //sadsafsdfsdfsdfsdfsdf
+    //215115
+
+
 }
+
+
+         // Create new java class under src folder
+         // make some changes
+         // push to remote repository
+
+          // git status
+         // 1. git add *
+         //  git status
+         // 2. git commit -m"your msg"
+            // git status
+         // 3. git push
+          //  git status
